f2052fd662206195c64967bf00f47acf9c49ee5e
bjing94 <kovriklol@gmail.com>
2022-05-19 14:28:30 +0300

    removed static serving

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -3,14 +3,9 @@ import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { FileModel } from './file.model';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { path } from 'app-root-path';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: `${path}/uploads`,
-    }),
     TypegooseModule.forFeature([
       {
         typegooseClass: FileModel,
